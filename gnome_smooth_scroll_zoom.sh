e7249ae3af0c3bb10e296de20f9bbba25494f02f
Muhammad Moneib <mmoneib@gmx.com>
2020-01-16 08:20:29 +0100

    Improved performance by restructuring gnome_smooth_scroll_zoom to run on 2 process; allowed running at startup; changed the key used to LEFTALT.

--- gnome_smooth_scroll_zoom.sh
+++ gnome_smooth_scroll_zoom.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 ###########################################################################################################
 # A script to magnify Gnome's screen based on the mouse wheel scrolling and several HW detection methods. #
-# The script modifies Gnome settings for the magnification.                                               #
+# The script modifies Gnome settings for the magnification. It forks a special process for mouse events   #
+# while keeping the original for keyboard events, which are more scarce and so, less process intensive.   #
+# Inter-process communication is minimal and limited to signalling in order to avoid writing into the     #
+# file system.                                                                                            #
+# The script is designed also to run as a startup background application. To do so, one should add an     #
+# exception in the sudoers file to allow sudo the script without a password. In that case, the script     #
+# should have execution and writing priviliges assigned only to root. Make sure to edit using visudo.     #
+#   example: your_username_here ALL=(ALL) NOPASSWD: /usr/bin/gnome_smooth_scroll_zoom.sh                  #
 # HW events detection is based on:                                                                        #
 #   libinput: based on the unrecommended debug output. Sluggish, excessive, and subject to change.        #
 # Developed by: Muhammad Moneib                                                                           #
 ###########################################################################################################
 
+kbd=;
 magOnOff=false;
 magFactor=1.0;
+user=$(who|cut -d ' ' -f 1); # Parses the current user's name.
+userID=$(id -u $user); # Gets the user's numeric ID,
 
 function initializeGnomeMagnifierSettings {
   magFactor=1.0;
-  gsettings set org.gnome.desktop.a11y.magnifier mag-factor $magFactor;
+  # The gsettings program needs to be run as the user and not root. Also, it requires the address' env variable. Using sude -E wasn't enough due to inner references.
+  sudo -u $user DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$userID/bus" gsettings set org.gnome.desktop.a11y.magnifier mag-factor $magFactor;
   switchGnomeMagnifierOnOff;
 }
 
@@ -23,36 +34,53 @@ function switchGnomeMagnifierOnOff {
   else
     magOnOff=false;
   fi
-  gsettings set org.gnome.desktop.a11y.applications screen-magnifier-enabled $magOnOff;
+  sudo -u $user DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$userID/bus" gsettings set org.gnome.desktop.a11y.applications screen-magnifier-enabled $magOnOff;
+  if [[ $magOnOff == false ]]; then
+    pkill -P $$; # Kill current process and all its children.
+  fi
 }
 
-function zoomWithLibinput {
-    function zoom {
-      while read -r inp; do
-        if [[ ! -z $(echo "$inp" | grep -E "KEYBOARD.*pressed") ]]; then # .* to grep with AND.
-          magMode=true;
-        elif [[ ! -z $(echo "$inp" | grep -E "KEYBOARD.*released") ]]; then
-          magMode=false;
-        elif [[ $magMode == true ]] && [[ ! -z $(echo "$inp" | grep "vert -") ]]; then
-          magFactor=$(echo "scale=1;$magFactor+0.1"|bc); # The tool bc is used for decimals calc.
-        elif [[ $magMode == true ]] && [[ ! -z $(echo "$inp" | grep "vert") ]]; then
-          magFactor=$(echo "scale=1;$magFactor-0.1"|bc);
-          if (($(echo "$magFactor<1"|bc)==1)); then
-            magFactor=1;
-          fi
-        fi 
-        gsettings set org.gnome.desktop.a11y.magnifier mag-factor $magFactor;
-      done
-  }
-  # stdbuf is used to remove the buffering and allow detection of the continuous stream.
-  sudo libinput debug-events --show-keycodes | stdbuf -oL grep "wheel\|KEY_LEFTCTRL" | zoom; # \| to grep with OR. 
+function zoomMouseEventDetect { # Forked and piped, so all (even global which are copied) variables are limited to its scope.
+  while read -r inp; do
+    if [[ ! -z $(echo "$inp" | grep "vert -") ]]; then
+      magFactor=$(echo "scale=1;$magFactor+0.1"|bc); # The tool bc is used for decimals calc.
+    elif  [[ ! -z $(echo "$inp" | grep "vert") ]]; then
+      magFactor=$(echo "scale=1;$magFactor-0.1"|bc);
+      if (($(echo "$magFactor<1"|bc)==1)); then
+        magFactor=1;
+      fi
+    fi 
+    sudo -u $user DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$userID/bus" gsettings set org.gnome.desktop.a11y.magnifier mag-factor $magFactor;
+  done
+}
+
+function zoomKeyboardEventDetect {
+  pkill --signal STOP -P $!; # Default action is to prevent zooming.
+  while read -r inp1; do
+    if [[ ! -z $(echo "$inp1" | grep -E "KEYBOARD.*pressed") ]]; then # .* to grep with AND.      
+      pkill --signal CONT -P $!;
+    elif [[ ! -z $(echo "$inp1" | grep -E "KEYBOARD.*released") ]]; then
+      pkill --signal STOP -P $!; # Interrupt forked child process. (keeps values of variables as the process won't die)
+    fi
+  done
+}
+
+function tryToDetectKeyboard {
+  kbd=$(ls -l /dev/input/by-path/ | grep kbd | tail -1); # $() executes the enclosed command and put the output in a variable.  
+  kbd=${kbd:((${#kbd}-1)):${#kbd}};
+  if [[ ! -z $kbd ]]; then
+    kbd="/dev/input/event"$kbd;
+  fi
 }
 
 trap initializeGnomeMagnifierSettings EXIT;
 initializeGnomeMagnifierSettings;
-zoomWithLibinput;
+tryToDetectKeyboard;
+# stdbuf is used to remove the buffering and allow detection of the continuous stream.
+(stdbuf -oL libinput debug-events | stdbuf -oL grep "wheel" | zoomMouseEventDetect)& # The & is for forking the child process as the mouse detection of libinput produces more events.
+stdbuf -oL libinput debug-events $kbd --show-keycodes | stdbuf -oL grep "KEY_LEFTALT" | zoomKeyboardEventDetect; # \| to grep with OR. 
 
-#TODO Listen to the keyboard only until CTRL is pressed. (performance)
-#TODO Try to combine the streams from the keyboard and mouse only for libinput
-#TODO Add other backbones than libinput.
-#TODO Investigate why libinput's output only comes after mouse movement (even for keyboard!)
+#TODO Add hot support for lense mode.
+#TODO Add touchpad and touch screen pinching support.
+#TODO Add other backends other than libinput.
+#TODO Add event number parameters for forcing speciific mouse and keyboard.
