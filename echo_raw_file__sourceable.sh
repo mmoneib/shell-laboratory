3afc3766daf2ee6d5747e5b463ec043743318740
Muhammad Moneib <mmoneib@gmx.com>
2020-02-19 00:07:10 +0100

    Added printing of indifinitely repeatable elements in for Echo Raw File script; refactored.

--- echo_raw_file__sourceable.sh
+++ echo_raw_file__sourceable.sh
@@ -15,6 +15,7 @@ declare -A repetitionMap; # Hashmap
 declare -A referenceRepetitionMap; # Hashmap to kepp the original values of repetitions immutable.
 declare -A sizeMap; # Hashmap
 declare -A levelMap; # Hashmap
+declare -A endMarksMap; # Hashmap, reversed by mapping the mark to the element.
 
 cop_getValueForOption "t"
 _erf_template=cat $cop_optionValue;
@@ -24,6 +25,8 @@ cop_doesThisOptionExist "test";
 _erf_isTest=$cop_hasOption;
 cop_doesThisOptionExist "verbose_out";
 _erf_isVerboseOut=$cop_hasOption;
+
+buffer=""; # Used by multiple functions. Therefore, global.
 
 function buildFileTree {
   currentParents=();
@@ -37,6 +40,7 @@ function buildFileTree {
     currentLevel=$(cut -d ',' -f 1 <<< $inp);
     elementSize=$(cut -d ',' -f 3 <<< $inp);
     elementRepeatability=$(cut -d ',' -f 4 <<< $inp);
+    elementEndMark=$(cut -d ',' -f 5 <<< $inp);
     currentParents[$currentLevel]=$elementName; # Can overwrite.
     parent=${currentParents[(($currentLevel-1))]}; # The parent from the level above.
     if [[ ! -z $parent ]]; then
@@ -45,6 +49,9 @@ function buildFileTree {
     repetitionMap[$elementName]=$([[ -z $elementRepeatability ]] && echo 1 || echo $elementRepeatability);
     sizeMap[$elementName]=$elementSize;
     levelMap[$elementName]=$currentLevel;
+    if [[ ! -z $elementEndMark ]]; then
+      endMarksMap[$elementName]=$elementEndMark;
+    fi
   done
 } <<< $_erf_template;
 
@@ -64,30 +71,42 @@ function prepareForDfs {
 function echoFile { # Expects a string streamed by <<< or piped.
   for ((i=0;i<${#dfs_orderedExecution[@]};i++)) do
     element=${dfs_orderedExecution[$i]};
-    sizeToRead=${sizeMap[$element]};
+    elementSize=${sizeMap[$element]};
+
     if ((${#childrenMap[$element]}==0)); then # Size only matters for the leaf nodes. The rest is discarded.
-        count=${repetitionMap[$element]}; # Repetitions fpr leaf nodes are dealt with immediately, so they need not keep a state.
-       while (( $count >= 1 )); do
-          read -N $sizeToRead out;
-          if [[ $_erf_isVerboseOut == true ]]; then
-            echo $element
+      count=${repetitionMap[$element]}; # Repetitions fpr leaf nodes are dealt with immediately, so they need not keep a state.
+      if [[ "$count" == "0" ]]; then
+        readElement $element;
+      else
+        while [[ "$count" == "n" ]] || (( $count >= 1 )); do
+          if [[ "$count" == "n" ]]; then # Indefinite repetitions.
+            peekText=${endMarksMap[$element]};
+            checkMarkedElement $peekText;
+            if [[ $markedElementReached == true ]]; then
+              markedElementReached=false;
+              count=-1;
+              break;
+            fi
+            readElement $element;
+          else
+            readElement $element;
+            count=$(($count-1)); 
           fi
-          echo $out;
-          count=$(($count-1)); 
        done
+      fi
     else
-      repetitionMap[$element]=$((${repetitionMap[$element]}-1)); 
-      if [[ "${repetitionMap[$element]}" == "n" ]]; then
-        echo "Repeat indefinitely or till a mark." #TODO
-      elif (( ${repetitionMap[$element]} >= 1 )); then 
+      if [[ "${repetitionMap[$element]}" != "n" ]]; then
+        repetitionMap[$element]=$((${repetitionMap[$element]}-1)); 
+      fi
+      if [[ "${repetitionMap[$element]}" == "n" ]] || (( ${repetitionMap[$element]} >= 1 )); then
         elementLevel=${levelsMap[$element]};
         tempLevel=${levelMap[${dfs_orderedExecution[$i]}]};
         for ((j=$i;j>=0;j--)); do
-          if (($j==0 || ${levelMap[${dfs_orderedExecution[$((j-1))]}]}<=$tempLevel)); then
+          if (($j==0 || ${levelMap[${dfs_orderedExecution[$((j-1))]}]}<=$tempLevel)); then # Go back until you reach a higher level.
             i=$(($j-1));
             break;
           fi
-        done	
+        done
       else
         repetitionMap[$element]=${referenceRepetitionMap[$element]}; # Reset for case of non-leaf node to be repeated due to parent.
       fi
@@ -95,12 +114,47 @@ function echoFile { # Expects a string streamed by <<< or piped.
   done
 } <<< $_erf_static;
 
+function checkMarkedElement {
+  mark=$1;
+  if (( ${#buffer} < ${#mark} )); then
+    read -N $((${#mark}-${#buffer} )) tmpOut;
+    buffer=$buffer$tmpOut;
+  fi
+  if [[ "${buffer:0:${#mark}}" == "$mark" ]]; then
+    markedElementReached=true;
+  fi
+}
+
+function readElement { # Reads an element while taking into consideration what's inside the buffer, if any.
+  sizeToRead=$((${sizeMap[$1]}-${#buffer})); # What's left to read for this element aside from what's in the buffer.
+  if (( $sizeToRead > 0 )); then
+    read -N $sizeToRead tmpOut;
+    out=$buffer$tmpOut;
+  else
+    sizeToRead=${sizeMap[$1]};
+    out=${buffer:0:${sizeToRead}};
+  fi
+  if [[ $out == "" ]]; then
+    exit;
+  fi
+  buffer=${buffer:$sizeToRead};
+  echoElement $1;
+  echo $out;
+}
+
+function echoElement {
+  if [[ $_erf_isVerboseOut == true ]]; then
+    echo $1
+  fi
+}
+
 if [[ $_erf_isTest == true ]]; then
   _erf_template="1,Header,2,
-1,Body,,
+1,Body,,,
 2,Chunk,,6
 3,ChunkHeader,1,
-3,Data,4,2
+3,Data,2,n,01010
+3,Chunk_EndMark,5,,
 1,Tailer,,
 2,TailerChunk,,8
 3,TailerChunkHeader,3,
@@ -111,7 +165,7 @@ if [[ $_erf_isTest == true ]]; then
   copyAssociativeArray;
   prepareForDfs;
   echo "Order of execution is ${dfs_orderedExecution[@]}."
-  _erf_static="00110001000000000000000001000000001110101000000001111111110101001001010101011110010010010101000101001101010001010101010010101000000000000000000001010101110101011000101010100001010111010101111111111110111100000011101010100101010011";
+  _erf_static="0010101010001000000000000000001000000001110101000000001111111110101001001010101011110010010010101000101001101010001010101010010101000000000000000000001010101110101011000101010100001010111010101111111111110111100000011101010100101010011";
   echoFile; 
   exit;
 fi
