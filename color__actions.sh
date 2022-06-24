9ad136bd8e8f21f4dfd838c6416e2254d2d80280
Muhammad Moneib <mmoneib@gmx.com>
2022-06-24 02:27:49 +0200

    Migrated color__actions to the new template; rename color and list actions.

--- color__actions.sh
+++ color__actions.sh
@@ -1,82 +1,111 @@
 #!/bin/sh
 ################################################################################
-# Color Utilities                                                              #
+# Color Actions                                                                #
 #                                                                              #
 # A set of utility functions to conveniently manipulate the colors of the text #
 # output inside the terminal as well as provide information about the          #
 # available colors.                                                            #
 #                                                                              #
-# Type: Utility.                                                               #
+# Type: Actions                                                                #
 # Dependencies: Unix-like Shell (tested with Bash), and tput (if not included. #
 # Developed by: Muhammad Moneib                                                #
 ################################################################################
 
-function usage {
-  echo "Usage: color_utilities__sourceable.sh -a action_here [-t text_here - color_number_here]
-        Available actions:
-          count_terminal_colors
-          print_all_colors_horizontally
-          print_all_colors_horizontally_with_nums
-          print_all_colors_vertically
-          print_all_colors_vertically_with_nums
-          print_text_with_color_and_background fg_color_num bg_color_num"
+# Positional parameters inside action functions are used especially for the case of sourcing.
+# Required parameters are denoted with the c_r_ prefix.
+# Optional parameters are denoted with the c_o_ prefix.
+
+function __print_usage {
+  sh $(dirname $0)/helc__actions.sh -a print_actions_usage_exiting -t $0
+}
+
+function __print_help {
+  sh $(dirname $0)/helc__actions.sh -a print_actions_help -t $0
+}
+
+function __print_missing_parameter_error {
+  echo "Validation Error: Missing the '$1' parameter required for this action.">&2
   exit 1
 }
 
-function _print_end_line {
+function __print_incorrect_action_error {
+  echo "Validation Error: The provided action is not available. Please check Help for more info.">&2
+  exit 1
+}
+
+function __print_end_line {
   printf "$(tput init)\n"
 }
 
+## Print the number of colors supported by the terminal.
 function count_terminal_colors {
   tput colors
 }
 
+## Print all colors supported by the terminal in order horizontally.
 function print_all_colors_horizontally {
   withNums=$1
-  for (( i=0; i<$(count_terminal_colors); i++ )); do
+  for ((i=0; i<$(count_terminal_colors);i++)); do
     printf "$(tput setab $i)"
     [ ! -z $withNums ] && [ $withNums = true ] && printf " $i " || printf ' '
   done
-  _print_end_line
+  __print_end_line
 }
 
-function  print_all_colors_horizontally_with_nums {
+## Print all colors supported by the terminal in order horizontally with the equivalent number on each.
+function print_all_colors_horizontally_with_nums {
   print_all_colors_horizontally true
 }
 
+## Print all colors supported by the terminal in order vertically.
 function print_all_colors_vertically {
   withNums=$1
-  for (( i=0; i<$(count_terminal_colors); i++ )); do
+  for ((i=0;i<$(count_terminal_colors);i++)); do
     printf "$(tput setab $i)"
     [ ! -z $withNums ] && [ $withNums = true ] && printf " $i " || printf ' '
-    _print_end_line
+    __print_end_line
   done
 }
 
+## Print all colors supported by the terminal in order vertically with the equivalent number on each.
 function print_all_colors_vertically_with_nums {
   print_all_colors_vertically true
 }
 
+## Print text in the screen with a specific font color and background color. 
 function print_text_with_color_and_background {
-  text=$1
-  color=$2
-  background=$3
-  tput setaf $color
-  tput setab $background
-  printf "$text"
-  _print_end_line
+  [ -z "$c_o_text" ] && __print_missing_parameter_error "text"
+  [ -z "$c_o_color" ] && __print_missing_parameter_error "color"
+  [ -z "$c_o_background" ] && __print_missing_parameter_error "background"
+  tput setaf "$c_o_color"
+  tput setab "$c_o_background"
+  printf "$c_o_text"
+  __print_end_line
 }
 
-if [ "$1" != "skip_run" ]; then # Escape condition for sourcing scipts.
-  __par=$1
-  if [ -z $1 ] || [ "${__par:0:1}" != "-" ] || (( ${#__par} < 2 )); then
-    usage
-  fi
-  
-  while getopts "a:" o; do
-    case $o in
-      a) $OPTARG ;;
-      ?) usage ;;
-    esac
+[ -z "$1" ] && __print_usage
+# Check if input is piped.
+read -t 0.1 inp; # Doesn't read more than a line.
+if [ ! -z "$inp" ]; then
+  c_o_text="$inp"
+  while read inp; do
+     c_o_text+="\n$inp"
   done
 fi
+# Parse options and parameters.
+while getopts "ha:b:c:t:" o; do
+  case $o in
+    ## The name of the function to be triggered.
+    a) c_r_action=$OPTARG ;;
+    ## The background to be preinted for the text.
+    b) c_o_background=$OPTARG ;;
+    ## The color of the text.
+    c) c_o_color=$OPTARG ;;
+    ## The text to be printed on the screen,
+    t) c_o_text=$OPTARG ;;
+    h) __print_help ;;
+    *) __print_usage ;;
+  esac
+done
+# Generic action call with protection against script injection.
+[ ! -z "$(grep "^function $c_r_action" $0)" ] && $c_r_action || __print_incorrect_action_error
--- list__actions.sh
+++ list__actions.sh
@@ -10,58 +10,65 @@
 # Developed by: Muhammad Moneib                                                #
 ################################################################################
 
-#TODO Add namespaces comment.
 # Positional parameters inside action functions are used especially for the case of sourcing.
-# Required parameters are denoted with the p_r_ prefix.
-# Optional parameters are denoted with the p_o_ prefix.
+# Required parameters are denoted with the c_r_ prefix.
+# Optional parameters are denoted with the c_o_ prefix.
 
 function __print_usage {
-  sh $(dirname $0)/help_actions.sh -a print_actions_usage_exiting -t $0
+  sh $(dirname $0)/helc__actions.sh -a print_actions_usage_exiting -t $0
 }
 
 function __print_help {
-  sh $(dirname $0)/help_actions.sh -a print_actions_help -t $0
+  sh $(dirname $0)/helc__actions.sh -a print_actions_help -t $0
+}
+
+function __print_missing_parameter_error {
+  echo "Validation Error: Missing the '$1' parameter required for this action.">&2
+  exit 1
+}
+
+function __print_incorrect_action_error {
+  echo "Validation Error: The provided action is not available. Please check Help for more info..">&2
+  exit 1
 }
 
 ## Gets the line of the specified number in a file or an output.
 function get_nth_line {
-  while read -t 0.1 inp; do # For the case of piping.
-    p_r_text+="$inp\n"
-  done
-  [ -z "$p_r_text" ] && p_r_text="$1"
-  [ -z "$p_o_lineNumber" ] && p_o_lineNumber="$2"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
-  echo -e "$p_r_text" | head -$p_o_lineNumber | tail -1 # The -e option is needed to preserve newline.
+  [ -z "$c_r_text" ] && __print_missing_parameter_error "text"
+  [ -f "$c_r_text" ] && c_r_text="$(cat $c_r_text)"
+  [ -z "$c_o_lineNumber" ] &&  __print_missing_parameter_error "line_number"
+  echo -e "$c_r_text" | head -"$c_o_lineNumber" | tail -1 # The -e option is needed to preserve newline.
 }
 
 ## Gets a random line in a file or an output.
 function get_random_line {
-  while read -t 0.1 inp; do # For the case of piping.
-    p_r_text+="$inp\n"
-  done
-  [ -z "$p_r_text" ] && p_r_text="$1"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
-  randomLineNumber="$(echo $((($RANDOM+1)% $(echo -e "$p_r_text"|wc -l))))" # Adding 1 to start from 1 instead 0 and to include the last line which equals to the file size.
-  echo -e "$p_r_text" | head -$randomLineNumber | tail -1 # The -e option is needed to preserve newline.
+  [ -z "$c_r_text" ] && __print_missing_parameter_error "text"
+  [ -f "$c_r_text" ] && c_r_text="$(cat $c_r_text)"
+  randomLineNumber="$(echo $((($RANDOM+1)% $(echo -e "$c_r_text"|wc -l))))" # Adding 1 to start from 1 instead 0 and to include the last line which equals to the file size.
+  echo -e "$c_r_text" | head -$randomLineNumber | tail -1 # The -e option is needed to preserve newline.
 }
 
-if [ "$1" != "skip_run" ]; then
-  if [ -z $1 ]; then
-    read -t 0.1 inp;
-    [ -z $inp ] && print_usage
-  fi
-  while getopts "ha:t:n:" o; do
-    case $o in
-      ## The name of the function to be triggered.
-      a) p_r_action=$OPTARG ;;
-      ## The number of the line within a text made of lines.
-      n) p_o_lineNumber=$OPTARG ;;
-      ## The text to be treated as a list.
-      t) p_r_text=$OPTARG ;;
-      h) __print_help ;;
-      *) __print_usage ;;
-    esac
+[ -z "$1" ] && __print_usage
+# Check if input is piped.
+read -t 0.1 inp; # Doesn't read more than a line.
+if [ ! -z "$inp" ]; then
+  c_r_text="$inp"
+  while read inp; do
+    c_r_text+="\n$inp"
   done
-  # Generic action call with positional parameters based on available ones.
-  $p_r_action "$p_r_text" "$p_o_lineNumber"
 fi
+# Parse options and parameters.
+while getopts "ha:t:n:" o; do
+  case $o in
+    ## The name of the function to be triggered.
+    a) c_r_action=$OPTARG ;;
+    ## The number of the line within a text made of lines.
+    n) c_o_lineNumber=$OPTARG ;;
+    ## The text to be treated as a list.
+    t) c_r_text=$OPTARG ;;
+    h) __print_help ;;
+    *) __print_usage ;;
+  esac
+done
+# Generic action call with positional parameters based on available ones.
+[ ! -z "$(grep "^function $c_r_action" $0)" ] && $c_r_action || __print_incorrect_action_error
