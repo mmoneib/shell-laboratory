6cb9e796918939f52f1ccc4361f1c67687016123
Muhammad Moneib <mmoneib@gmx.com>
2022-06-28 08:40:49 +0200

    Added truth_table__progressive_process.

---
+++ truth_table__progressive_process.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+usage="Usage: ."
+while getopts "n:c:v:w:h:s:mp" o; do
+  case "$o" in
+    m) isMultiplication="true" ;;
+    p) isCompounding="true" ;;
+    n) numOfIterations="$OPTARG" ;;
+    h) echo "Help: ." && exit ;;
+    w) weightsPar="$OPTARG" ;;
+    c) choicesPar="$OPTARG" ;;
+    v) valuesPar="$OPTARG" ;;
+    s) stopPattern="$OPTARG" ;;
+    *) echo "$usage">&2 && exit 1 ;;
+  esac
+done
+IFS=,
+read -a choices <<< "$choicesPar"
+read -a weights <<< "$weightsPar"
+read -a values <<< "$valuesPar"
+numOfChoices=${#choices[@]}
+numOfRows="$(echo "$numOfChoices^$numOfIterations"|bc)"
+elements=()
+output=""
+for((i=$numOfIterations;i>0;i--)); do
+  index=0
+  flipper="$(echo "($numOfChoices^$i)/2"|bc)"
+  for ((j=1;j<=$numOfRows;j++)); do
+    elements+=("${choices[$index]}")
+    if [ "$flipper" != "0" ] && [ "$(echo "$j%$flipper"|bc)" == "0" ]; then
+      index=$((($index+1)%${#choices[@]}))
+    fi
+  done
+done
+for((i=0;i<$numOfRows;i++)); do
+ for((j=0;j<$numOfIterations;j++)); do
+   index=$(((($i+1)+($j*$numOfRows))-1))
+   output+="${elements[$index]}"
+#   printf "${elements[$index]}"
+ done
+ output+="\n"
+# printf "\n"
+done
+#printf "$output"
+while read inp; do
+  if [ ! -z "$stopPattern" ]; then
+    inp="$(echo $inp|sed s/$stopPattern.*/$stopPattern/g)"
+  fi
+  equation=""
+  for((i=0;i<${#inp};i++)); do
+    choice=${inp:$i:1}
+    choiceIndex=-1
+    for((j=0;j<${#choices};j++)); do
+      [ "${choices[$j]}" == "$choice" ] && choiceIndex="$j" && break
+    done
+    value="${values[$choiceIndex]}"
+    weight="${weights[$i]}"
+    if [ "$isMultiplication" == "true" ]; then
+      equation+="($value*$weight)*"
+    elif [ "$isCompounding" == "true" ]; then
+      [ "${value:0:1}" == "-" ] && weight="$(echo "2-$weight"|bc -l)" && value="1"
+      equation+="($value*$weight)*"
+      #equation+="($value*$weight)"
+      #equation+="+($equation)*"
+    else
+      equation+="$value*$weight+"
+    fi
+  done
+  echo "$equation"
+  echo "$inp->$(echo $equation|sed s/\+$//g|sed s/\*$//g|bc -l)"
+done <<< "$(printf "$output")"
+
