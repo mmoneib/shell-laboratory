c2c7f514fa489d185ef63a35a53dd20a9498f063
Muhammad Moneib <mmoneib@gmx.com>
2023-01-21 03:02:50 +0100

    Added new actions to string__actions including rolling and validations.

--- string__actions.sh
+++ string__actions.sh
@@ -16,6 +16,12 @@
 #TODO Add ignore case to all suitable actions.
 #TODO Update README to reflect preference to internal calls.
 #TODO Add action to roll circularly for a given list (regex) of acceptable values.
+#TODO Add documentation of preference of multiple calls at runtime than increased complexity.
+#TODO Combine all rolling actions into one.
+#TODO Adding email address verification.
+#TODO Add case alternation.
+#TODO Add sorting of letters.
+#TODO Add calculated replacement of chars from list.
 
 function __print_usage {
   sh $(dirname $0)/help__actions.sh -a print_actions_usage -t $0
@@ -39,19 +45,19 @@ function __print_incorrect_action_error {
 
 ## Counts the number of timnes a specified single character appears in the supplied text. The search cab be flagged as case-insensitive.
 function count_char_occurrences {
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   [ -z "$p_o_character" ] && __print_missing_parameter_error "character"
   [ -z "$p_o_ignoreCase" ] && p_o_ignoreCase="false"
   cCount=0;
   if [ "$p_o_ignoreCase" == "true" ]; then
     p_o_character="$(echo $p_o_character|tr [:lower:] [:upper:])"
-    p_r_text="$(echo $p_r_text|tr [:lower:] [:upper:])"
+    p_o_text="$(echo $p_o_text|tr [:lower:] [:upper:])"
   fi
   # Using # inside ${} to get the size of the string.
-  for (( i=0;i<${#p_r_text};i++ )); do
+  for (( i=0;i<${#p_o_text};i++ )); do
     # Using $ to compare strings, as comparison of values when provide the correct equality.
-    if [[ "${p_r_text:i:1}" == "$p_o_character" ]]; then 
+    if [[ "${p_o_text:i:1}" == "$p_o_character" ]]; then 
       ((cCount=cCount+1))
     fi 
   done
@@ -60,8 +66,8 @@ function count_char_occurrences {
 
 ## Flips the case of each character in the supplied text.
 function flip_case {
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   flippedCaseText="";
   # Using # inside ${} to get the size of the string.
   IFS=""; while read l; do # IFS needed to preserve leading spaces.
@@ -76,21 +82,45 @@ function flip_case {
       fi 
     done
     flippedCaseText+="\n"
-  done <<< "$p_r_text"
+  done <<< "$p_o_text"
   printf "$flippedCaseText";
+}
+
+## Check if the provided string contains only alphabetic characters based on a range (defaults to [a-Z]).
+function is_string_alphabetic {
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ ! -z "$(echo $p_o_text | grep "^[A-Za-z]*$")" ] && echo "true" || echo "false"
+}
+
+## Check if the provided string contains numbers and characters based on a range (defaults to [a-Z]).
+function is_string_alphanumeric {
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ ! -z "$(echo $p_o_text | grep "^[0-9A-Za-z]*$")" ] && echo "true" || echo "false"
+}
+
+## Check if the provided string is a number.
+function is_string_number {
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ ! -z "$(echo $p_o_text | grep "^[0-9]*$")" ] && echo "true" || echo "false"
+}
+
+## Check if the provided string has a special character (any character which is not a number and not in the provided alphabetic range defaulting to [a-Z]).
+function is_string_with_special_character {
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ ! -z "$(echo $p_o_text | grep "[^0-9A-Za-z]")" ] && echo "true" || echo "false"
 }
 
 ## Keep the first and last letters only in each word of the supplied text.
 function remove_internal_chars {
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
-  printf "$p_r_text\n" | tr '\n' '\r' | sed "s/\([a-Z]\)[a-Z]*\([a-Z][^a-Z]\)/\1\2/g"|tr '\r' '\n' # The command tr is used as a workaround since sed doesn't consider \n outside [a-z].
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
+  printf "$p_o_text\n" | tr '\n' '\r' | sed "s/\([a-Z]\)[a-Z]*\([a-Z][^a-Z]\)/\1\2/g"|tr '\r' '\n' # The command tr is used as a workaround since sed doesn't consider \n outside [a-z].
 }
 
 ## Replaces each encountered placeholder {} with the field whose turn comes in the supplied separated list. The number of placeholders should be the same as the number of fields in the list.
 function remove_text { 
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   [ -z "$p_o_separatedListText" ] && __print_missing_parameter_error "separated_list_text"
   p_o_dictionary=""
   p_o_dictionary="$(printf "$p_o_separatedListText"|sed "s/\([^;]\)$/\1;/g"|sed "s/;/=;/g")"
@@ -99,8 +129,8 @@ function remove_text {
 
 ## Replaces each encountered placeholder {} with the field whose turn comes in the supplied separated list. The number of placeholders should be the same as the number of fields in the list.
 function replace_positional_placeholders { 
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   [ -z "$p_o_separatedListText" ] && __print_missing_parameter_error "separated_list_text"
   prefix_placeholder='{'
   postfix_placeholder='}'
@@ -122,16 +152,16 @@ function replace_positional_placeholders {
       buffer=""
     done
     outputText+="\n"
-  done <<< "$p_r_text"
+  done <<< "$p_o_text"
   printf "$outputText" # Using printf to have the same exact output as input in terms of formatting. The command 'echo' produces an extra line at the end.
 }
 
 ## Replaces each encountered text entry from the dictionaty with its corresponding text there.
 function replace_text_by_dictionary { 
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   [ -z "$p_o_dictionary" ] && __print_missing_parameter_error "dictionary"
-  outputText="$p_r_text"
+  outputText="$p_o_text"
   recordPos=1
   while [ ! -z "$(echo "$p_o_dictionary"|cut -d ";" -f $recordPos)" ]; do
     field=$(echo "$p_o_dictionary"|cut -d ';' -f $recordPos)
@@ -146,17 +176,76 @@ function replace_text_by_dictionary {
 
 ## Roll the characters of a string based on their numerical (decimal) value and the offset provided.
 function roll_chars_by_offset {
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   [ -z "$p_o_offset" ] && __print_missing_parameter_error "offset"
   outputText=""
-  for ((p=0;p<${#p_r_text};p++)); do 
-    c="${p_r_text:$p:1}"
+  for ((p=0;p<${#p_o_text};p++)); do 
+    c="${p_o_text:$p:1}"
     p_o_character="$c" # Preparing for internal call to another action.
     val="$(show_decimal_of_char $c)"
     newVal="$(($val+$p_o_offset))"
     newChar="$($0 -a show_char_of_decimal -c "$newVal")" # Called externally due to null bytes by printf output being not allowed in command substitution $(). Expensive. #TODO Use read or truncate null bytes.
     outputText+="$newChar"
+  done
+  printf "$outputText\n"
+}
+
+## Roll the characters of a string based on their numerical (decimal) value and the offset provided within the given boundaries (defaults to A-z, where z is followed by a to restart the cycle).
+function roll_chars_circularly_by_offset {
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
+  [ -z "$p_o_offset" ] && __print_missing_parameter_error "offset"
+  [ -z "$p_o_begin" ] && p_o_begin="a"
+  [ -z "$p_o_end" ] && p_o_end="z"
+  outputText=""
+  p_o_character=$p_o_begin
+  beginningValue="$(show_decimal_of_char)"
+  p_o_character=$p_o_end
+  endValue="$(show_decimal_of_char)"
+  for ((p=0;p<${#p_o_text};p++)); do 
+    c="${p_o_text:$p:1}"
+    p_o_character="$c" # Preparing for internal call to another action.
+    val="$(show_decimal_of_char $c)"
+    if [ $val -ge $beginningValue ] && [ $val -le $endValue ]; then 
+      newVal="$(($val+$p_o_offset))"
+      [ $newVal -gt $endValue ] && newVal="$(echo $beginningValue+$newVal%$endValue | bc)"
+      newChar="$($0 -a show_char_of_decimal -c "$newVal")" # Called externally due to null bytes by printf output being not allowed in command substitution $(). Expensive. #TODO Use read or truncate null bytes.
+      outputText+="$newChar"
+    else
+      outputText+="$c"
+    fi
+  done
+  printf "$outputText\n"
+}
+
+## Roll the characters of a string based on their numerical (decimal) value and the current iterated number of the sequence provided within the given boundaries (defaults to A-z, where z is followed by a to restart the cycle).
+function roll_chars_circularly_by_sequence {
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
+  [ -z "$p_o_sequence" ] && __print_missing_parameter_error "sequence"
+  [ -z "$p_o_begin" ] && p_o_begin="a"
+  [ -z "$p_o_end" ] && p_o_end="z"
+  outputText=""
+  p_o_character=$p_o_begin
+  beginningValue="$(show_decimal_of_char)"
+  p_o_character=$p_o_end
+  endValue="$(show_decimal_of_char)"
+  count=0
+  for ((p=0;p<${#p_o_text};p++)); do 
+    c="${p_o_text:$p:1}"
+    p_o_character="$c" # Preparing for internal call to another action.
+    val="$(show_decimal_of_char $c)"
+    if [ $val -ge $beginningValue ] && [ $val -le $endValue ]; then
+      offset="${p_o_sequence:$((count%${#p_o_sequence})):1}"
+      ((count++))
+      newVal="$(($val+$offset))"
+      [ $newVal -gt $endValue ] && newVal="$(echo $beginningValue+$newVal%$endValue | bc)"
+      newChar="$($0 -a show_char_of_decimal -c "$newVal")" # Called externally due to null bytes by printf output being not allowed in command substitution $(). Expensive. #TODO Use read or truncate null bytes.
+      outputText+="$newChar"
+    else
+      outputText+="$c"
+    fi
   done
   printf "$outputText\n"
 }
@@ -175,18 +264,18 @@ function show_decimal_of_char {
 
 ## Shows the positions (starting from 1) of the supplied single character in the supplied text. If multiple occurrences, the positions are separated by commas. The search can be flagged as case-insensitive.
 function show_positions_of_char {
-  [ -z "$p_r_text" ] && __print_missing_parameter_error "text"
-  [ -f "$p_r_text" ] && p_r_text="$(cat $p_r_text)"
+  [ -z "$p_o_text" ] && __print_missing_parameter_error "text"
+  [ -f "$p_o_text" ] && p_o_text="$(cat $p_o_text)"
   [ -z "$p_o_character" ] && __print_missing_parameter_error "character"
   [ -z "$p_o_ignoreCase" ] && p_o_ignoreCase="false"
   if [ "$p_o_ignoreCase" == "true" ]; then
     p_o_character="$(echo "$p_o_character"|sed "s/\(.\)/\U\1/g")"
-    p_r_text="$(echo "$p_r_text"|sed "s/\(.\)/\U\1/g")"
+    p_o_text="$(echo "$p_o_text"|sed "s/\(.\)/\U\1/g")"
   fi
   posText=""
   # Using # inside ${} to get the size of the string.
-  for ((i=0; i<${#p_r_text}; i++)); do 
-    if [[ "${p_r_text:i:1}" == "$p_o_character" ]]; then 
+  for ((i=0; i<${#p_o_text}; i++)); do 
+    if [[ "${p_o_text:i:1}" == "$p_o_character" ]]; then 
       posText+="$((i+1)),"
     fi 
   done
@@ -198,33 +287,39 @@ function show_positions_of_char {
 # Check if input is piped.
 read -t 0.1 inp; # Doesn't read more than a line.
 if [ ! -z "$inp" ]; then
-  p_r_text="$inp"
+  p_o_text="$inp"
   while read inp; do
-    p_r_text+="\n$inp"
+    p_o_text+="\n$inp"
   done 
 fi
 # Parse options and parameters.
-while getopts "ha:c:d:il:o:t:" o; do
+while getopts "ha:b:c:d:e:il:o:s:t:" o; do
   case $o in
     ## The name of the function to be triggered.
     a) p_r_action=$OPTARG ;;
+    ## Beginning position or character ;;
+    b) p_o_begin=$OPTARG ;;
     ## The character to be queried or manipulated. The size must be of a single character.
     c) p_o_character=$OPTARG ;;
     ## A list of keys and values that resembles to a dictionary. The record separator defaults to semi-colon, while the key/value separator is '='.
     d) p_o_dictionary="$OPTARG" ;;
-    ## A flag to become case-insensitive while querying. If the option is present, the flag is true; otherwise, false.
+    ## Ending position or character ;;
+    e) p_o_end=$OPTARG ;;    ## A flag to become case-insensitive while querying. If the option is present, the flag is true; otherwise, false.
     i) p_o_ignoreCase="true" ;;
     ## A string in the form of a list separated by the serparator, which defaults to ai semi-colon.
     l) p_o_separatedListText=$OPTARG ;;
     ## Offset number indicating the distance between two elements in the string.
     o) p_o_offset=$OPTARG ;;
      #  [ "$OPTARG" -eq "$(echo $OPTARG | grep "^[0-9]*$")" ] || echo "ERROR: Incorrect parameter value for o." ;;
+    ## A sequence of numbers used in calculated string manipulations.
+    s) p_o_sequence=$OPTARG ;;
     ## The text to be queried or manipulated. This can be a string specified via command line, or a path to a text file.
-    t) p_r_text="$OPTARG" ;;
+    t) p_o_text="$OPTARG" ;;
     h) __print_help ;;
     *) __print_usage ;;
   esac
 done
+[ -z "$p_r_action" ] && __print_missing_parameter_error
 
 # Generic action call with protection against script injection.
 [ ! -z "$(grep "^function $p_r_action" $0)" ]  && $p_r_action || __print_incorrect_action_error
