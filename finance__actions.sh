21c572884344b25493fb2e75d39b71f97e8a5a04
Muhammad Moneib <mmoneib@gmx.com>
2023-09-14 00:28:13 +0200

    Added calculation of break even to finance__actions.

--- finance__actions.sh
+++ finance__actions.sh
@@ -6,13 +6,15 @@
 #                                                                              #
 # Type: Actions                                                                #
 # Dependencies: Unix-like Shell (tested with Bash)                             #
-#     ~additional dependenies here~                                            #
+#     bc.                                                                      #
 # Developed by: Muhammad Moneib                                                #
 ################################################################################
 
 # Positional parameters inside action functions are used especially for the case of sourcing.
 # Required parameters are denoted with the p_r_ prefix.
 # Optional parameters are denoted with the p_o_ prefix.
+
+# TODO Add validation of the values.
 
 function __print_usage {
   sh $(dirname $0)/help__actions.sh -a print_actions_usage -t $0
@@ -34,35 +36,36 @@ function __print_incorrect_action_error {
   exit 1
 }
 
-## ~comment describing the action here~
-function ~action function name here~ {
-  [ -z "~parameter variable here~" ] && __print_missing_parameter_error ~parameter_name_here~
-  ~further validation of optional parameters or for types here~
-  ~repitition~
-  ~repitition~
-  ~action implementation here~
+## Calculate breakeven factor. The amount provided should be between 0 and 1 and represents the loss portion (percentage in decimals).
+function calculate_breakeven_factor {
+  [ -z "$p_r_operand" ] && __print_missing_parameter_error "operand"
+  result="$(echo "scale=2;1/(1-$p_r_operand)"|bc -l)"
+  echo "$result"
 }
 
-~repitition~
+## Calculate breakeven factor. The amount provided should be between 0 and 1 and represents the loss portion (percentage in decimals).
+function calculate_breakeven_percentage {
+  result="$(calculate_breakeven_factor)"
+  result="$(echo "scale=2;$result-1"|bc -l)"
+  printf "%.2f\n" "$result" # FOrmatting using prontf to ensure leading zero.
+}
 
 [ -z "$1" ] && __print_usage
 # Check if input is piped.
 read -t 0.1 inp; # Doesn't read more than a line.
 if [ ! -z "$inp" ]; then
-  ~input text parameter here~="$inp"
+  p_r_operand="$inp"
   while read inp; do
-     ~input text parameter here~+="\n$inp"
+     p_r_operand+="\n$inp"
   done
 fi
 # Parse options and parameters.
-while getopts "ha:~getopts parameter string here~" o; do
+while getopts "ha:o:" o; do
   case $o in
     ## The name of the function to be triggered.
     a) p_r_action=$OPTARG ;;
-    ~description of parameter here~
-    ~parameter character here~) ~parameter variable here~=$OPTARG ;;
-    ~repitition~
-    ~repitition~
+    ## Amount as an operand on which the calculation is done.
+    o) p_r_operand=$OPTARG ;;
     h) __print_help ;;
     *) __print_usage ;;
   esac
