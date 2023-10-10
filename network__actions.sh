23b459f370f35cdfcbce4a9ac1a471ccee4f6b89
Muhammad Moneib <mmoneib@gmx.com>
2023-10-10 08:52:34 +0200

    Added get_ip_and_hostname_of_devices_connected_to_current_subnet and get_port_and_protocol_of_devicesto network__actions.

--- network__actions.sh
+++ network__actions.sh
@@ -54,12 +54,27 @@ function get_ip_with_mask_of_current_subnet {
   echo "$(ip route|grep default -A 1|head -2|tail -1|cut -d " " -f 1)"
 }
 
+## Get space-separated IP and the hostname (if available) of the devices connected to the current subnet.
+function get_ip_and_hostname_of_devices_connected_to_current_subnet {
+  subnetAddress=$(get_ip_with_mask_of_current_subnet)
+  output="$(nmap -sn $subnetAddress |grep "Nmap scan report for "|sed 's/Nmap scan report for //g'|sed 's/\(.*\)\ (\(.*\))/\2 \1/g')" # sn for a Ping Scan.
+  echo "$output"
+}
+
+## Get space-separated port and protocol of a specific device whose IP is provided.
+function get_port_and_protocol_of_devices {
+  [ -z "$p_o_ip" ] && __print_missing_parameter_error "ip"
+  output="$(nmap -p "*" $p_o_ip |grep "open"|sed 's/\(.*\)\/\(.*\)\ \(.*\)\ \(.*\)/\1 \2/g')"
+  echo "$output"
+}
+
 [ -z "$1" ] && __print_usage
 # Parse options and parameters.
-while getopts "ha:~getopts parameter string here~" o; do
+while getopts "ha:i:" o; do
   case $o in
     ## The name of the function to be triggered.
     a) p_r_action=$OPTARG ;;
+    i) p_o_ip=$OPTARG ;;
     h) __print_help ;;
     *) __print_usage ;;
   esac
