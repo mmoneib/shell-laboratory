282f949ab8b9f86eb3e24577283897cf8004d8ba
Muhammad Moneib <mmoneib@gmx.com>
2025-01-09 21:52:25 +0100

    Added generation of random sequence to random__actions.

--- random__actions.sh
+++ random__actions.sh
@@ -58,6 +58,63 @@ function arithmetic_random_walk_aggregate {
   echo $aggregation
 }
 
+## Generate a random sequence out of the choices provided and with the specified separator inbetween.
+function generate_random_sequence {
+  [ -z "$p_o_separatedChoicesText" ] && __print_missing_parameter_error "separated_choices_text"
+  [ -z "$p_o_numberOfTries" ] && __print_missing_parameter_error "number_of_tries"
+  [ -z "$p_o_separator" ] && [ "$p_o_separator" != "" ] && __print_missing_parameter_error "separator"
+  IFS=","; read -a choices <<< "$p_o_separatedChoicesText"
+  numberOfChoices=${#choices[@]}
+  count=0
+  output=""
+  while [ $count -lt $p_o_numberOfTries ]; do
+    choice="${choices[(( RANDOM%numberOfChoices ))]}"
+    output+="$choice$p_o_separator"
+    (( count++ ))
+  done
+  echo "$output"
+}
+
+## Generate a random sequence out of mixed case english alphabet with digits and with the specified separator inbetween.
+function generate_random_sequence_of_chars_and_digits {
+  [ -z "$p_o_numberOfTries" ] && __print_missing_parameter_error "number_of_tries"
+  [ -z "$p_o_separator" ] && [ "$p_o_separator" != "" ] && __print_missing_parameter_error "separator"
+  p_o_separatedChoicesText="q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,,b,n,m,Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,0,1,2,3,4,5,6,7,8,9"
+  generate_random_sequence
+}
+
+## Generate a random sequence out of lower case english alphabet and with the specified separator inbetween.
+function generate_random_sequence_of_chars_lower_case {
+  [ -z "$p_o_numberOfTries" ] && __print_missing_parameter_error "number_of_tries"
+  [ -z "$p_o_separator" ] && [ "$p_o_separator" != "" ] && __print_missing_parameter_error "separator"
+  p_o_separatedChoicesText="q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,,b,n,m"
+  generate_random_sequence
+}
+
+## Generate a random sequence out of mixed case english alphabet and with the specified separator inbetween.
+function generate_random_sequence_of_chars_mixed_case {
+  [ -z "$p_o_numberOfTries" ] && __print_missing_parameter_error "number_of_tries"
+  [ -z "$p_o_separator" ] && [ "$p_o_separator" != "" ] && __print_missing_parameter_error "separator"
+  p_o_separatedChoicesText="q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,,b,n,m,Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M"
+  generate_random_sequence
+}
+
+## Generate a random sequence out of upper case english alphabet and with the specified separator inbetween.
+function generate_random_sequence_of_chars_upper_case {
+  [ -z "$p_o_numberOfTries" ] && __print_missing_parameter_error "number_of_tries"
+  [ -z "$p_o_separator" ] && [ "$p_o_separator" != "" ] && __print_missing_parameter_error "separator"
+  p_o_separatedChoicesText="Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M"
+  generate_random_sequence
+}
+
+## Generate a random sequence out of digits and with the specified separator inbetween.
+function generate_random_sequence_of_digits {
+  [ -z "$p_o_numberOfTries" ] && __print_missing_parameter_error "number_of_tries"
+  [ -z "$p_o_separator" ] && [ "$p_o_separator" != "" ] && __print_missing_parameter_error "separator"
+  p_o_separatedChoicesText="0,1,2,3,4,5,6,7,8,9"
+  generate_random_sequence
+}
+
 ## Calculate product of a random walk based on numerical choices added to an aggregate with each iteration. Choices must be numerical.
 function geometric_random_walk_product {
   #echo "$(__has_decimal_point $p_o_separatedChoicesText)"
@@ -76,14 +133,16 @@ function geometric_random_walk_product {
 
 [ -z "$1" ] && __print_usage
 # Parse options and parameters.
-while getopts "ha:c:n:" o; do
+while getopts "ha:c:n:s:" o; do
   case $o in
     ## The name of the function to be triggered.
     a) p_r_action=$OPTARG ;;
-    ## List of choices separated by a comma.
-    c) p_o_separatedChoicesText=$OPTARG ;;
     ## Number of tries or iterations.
     n) p_o_numberOfTries=$OPTARG ;;
+    ## List of choices separated by a comma.
+    c) p_o_separatedChoicesText=$OPTARG ;;
+    ## Separator to separate entries in an output sequence.
+    s) p_o_separator=$OPTARG ;;
     h) __print_help ;;
     *) __print_usage ;;
   esac
