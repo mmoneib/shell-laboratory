ed4110ba5c4a722de35b0e8d9a079dae538c2d76
Muhammad Moneib <mmoneib@gmx.com>
2025-01-12 21:56:14 +0100

    Updated canvas to follow the yielding process template.

--- canvas__yielding_process.sh
+++ canvas__yielding_process.sh
@@ -22,57 +22,68 @@
 #TODO Add support for animation frames.
 #TODO Add options dialog.
 
-. ./bind
-tput clear;
-horPos=0;
-verPos=0;
+#. ./bind
 
 function exitFunc {
  # Put the cursose at the lowest line in the screen and keep the text present.
  tput cup $(tput lines) 0;
 }
 
-trap exitFunc EXIT;
+function initialize_input {
+  # Prevent parsing of input. Mainly for printing space here.
+  IFS=''
+  # Prevent echoing control characters. Mainly when reading is done, pressing arrows would print control.
+  stty -echoctl
+  tput clear;
+  d_horPos=0;
+  d_verPos=0;
+ trap exitFunc EXIT;
+}
 
-# Prevent parsing of input. Mainly for printing space here.
-IFS=''
-# Prevent echoing control characters. Mainly when reading is done, pressing arrows would print control.
-stty -echoctl
-
-while read -rsn1 inp; do
-    case "$inp" in # n1: reads 1 char only. s: silent. 
-    $'\033' ) # To check for ANSI escape char, we can also use x1b instead of 033. The $ is important to ensure you are looking for the escaping pattern.
-      read -rsn2 inp2;
-      case "$inp2" in
-      "[A") 
-      if [ $verPos == 0 ]; then # Prevents breaking top barrier.
-        verPos=1
-      fi
-      tput cup $((--verPos)) $horPos; #UP
+function process_data {
+  while read -rsn1 inp; do
+      case "$inp" in # n1: reads 1 char only. s: silent. 
+      $'\033' ) # To check for ANSI escape char, we can also use x1b instead of 033. The $ is important to ensure you are looking for the escaping pattern.
+        read -rsn2 inp2;
+        case "$inp2" in
+        "[A") 
+        if [ $d_verPos == 0 ]; then # Prevents breaking top barrier.
+          d_verPos=1
+        fi
+        tput cup $((--d_verPos)) $d_horPos; #UP
+          ;;
+        "[B")
+        if [ $d_verPos == $(($(tput lines)-1)) ]; then # Prevents breaking bottom barrier.
+          d_verPos=$(($(tput lines)-2))
+        fi
+        tput cup $((++d_verPos)) $d_horPos; #DOWN
+          ;;
+        "[C") 
+        if [ $d_horPos == $(($(tput cols)-1)) ]; then # Prevents breaking left barrier.
+          d_horPos=$(($(tput cols)-2))
+        fi
+        tput cup $d_verPos $((++d_horPos)); #RIGHT
+          ;;
+        "[D") 
+        if [ $d_horPos == 0 ]; then # Prevents breaking left barrier.
+          d_horPos=1
+        fi
+        tput cup $d_verPos $((--d_horPos)); #LEFT
+          ;;
+        esac
+        # No flushing of the rest of the control symbol is needed since the original loop reads one char at a time till it finds \033.
         ;;
-      "[B")
-      if [ $verPos == $(($(tput lines)-1)) ]; then # Prevents breaking bottom barrier.
-        verPos=$(($(tput lines)-2))
-      fi
-      tput cup $((++verPos)) $horPos; #DOWN
-        ;;
-      "[C") 
-      if [ $horPos == $(($(tput cols)-1)) ]; then # Prevents breaking left barrier.
-        horPos=$(($(tput cols)-2))
-      fi
-      tput cup $verPos $((++horPos)); #RIGHT
-        ;;
-      "[D") 
-      if [ $horPos == 0 ]; then # Prevents breaking left barrier.
-        horPos=1
-      fi
-      tput cup $verPos $((--horPos)); #LEFT
-        ;;
+        * )
+        o_char="$inp"
+        output
+        d_horPos=$(($d_horPos+1));
       esac
-      # No flushing of the rest of the control symbol is needed since the original loop reads one char at a time till it finds \033.
-      ;;
-      * )
-      printf "$inp";
-      horPos=$(($horPos+1));
-    esac
-done
+  done
+}
+
+function output {
+  printf "$inp"
+}
+
+initialize_input "$@"
+process_data
