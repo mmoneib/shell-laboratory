c64aa38046f69749382f957faf679fc1c117dee9
Muhammad Moneib <mmoneib@gmx.com>
2025-04-02 23:55:23 +0200

    Add listing all tags to tag__actions; refactoring.

--- tag__actions.sh
+++ tag__actions.sh
@@ -39,12 +39,12 @@ function __print_incorrect_action_error {
 ## Appends the specified list of tags to the specified file's metadata.
 function add_tag_to_file {
   [ -z "$p_r_path" ] &&  __print_missing_parameter_error "path"
-  [ -z "$p_r_tags" ] &&  __print_missing_parameter_error "tags"
-  tagFilePath="$(dirname $p_r_path)/.$(basename $p_r_path)__tags"
+  [ -z "$p_o_tags" ] &&  __print_missing_parameter_error "tags"
+  tagFilePath="$(dirname $p_r_path)/.$(basename $p_r_path)$tagsFilePostfix"
   if [ ! -f "$tagFilePath" ]; then
     touch $tagFilePath
   fi
-  IFS=,; read -a tagsArr <<< "$p_r_tags"
+  IFS=,; read -a tagsArr <<< "$p_o_tags"
   newTagsStr="$separator"
   for (( i=0; i<${#tagsArr[@]}; i++ )); do
     if [ ! -z "${tagsArr[i]}" ] && [ -z "$(grep "$separator${tagsArr[i]}$separator" $tagFilePath)" ]; then # Validate the tag and prevent duplication.
@@ -63,24 +63,31 @@ function add_tag_to_file {
 ## Search for files whose metadata include the specified tags.
 function find_files_with_tags {
   [ -z "$p_r_path" ] &&  __print_missing_parameter_error "path"
-  [ -z "$p_r_tags" ] &&  __print_missing_parameter_error "tags"
-  sortedTagsStr="$(echo "$p_r_tags"|tr ',' '\n'|sort|tr '\n' ',')"
+  [ -z "$p_o_tags" ] &&  __print_missing_parameter_error "tags"
+  sortedTagsStr="$(echo "$p_o_tags"|tr ',' '\n'|sort|tr '\n' ',')"
   IFS=,; read -a tagsArr <<< "$sortedTagsStr"
-  queryCommand="grep -H '$separator${tagsArr[0]}$separator' $p_r_path/.*__tags 2>/dev/null" # Divert error messages away.
+  queryCommand="grep -H '$separator${tagsArr[0]}$separator' $p_r_path/.*$tagsFilePostfix 2>/dev/null" # Divert error messages away.
   for (( i=1; i<${#tagsArr[@]}; i++ )); do
     queryCommand+="|grep '$separator${tagsArr[i]}$separator'" # Single quotes to deny scrip tinjection.
   done
-  queryCommand+="|cut -d : -f 1"
+  queryCommand+="|cut -d : -f 1|sed 's/\/\.\(.*\)$tagsFilePostfix/\/\1/g'"
   echo "Executing the command $queryCommand"
   eval "$queryCommand"
+}
+
+## Show the combined sorted list of all unique tags used across all files in the specified directory.
+function list_all_tags {
+  [ -z "$p_r_path" ] &&  __print_missing_parameter_error "path"
+  tagsList="$(cat $p_r_path"/".*$tagsFilePostfix|tr -d '\n'|tr ',' '\n'|sort|uniq)"
+  echo "${tagsList:1}" # Skipping the first character which is an empty line.
 }
 
 ## Remove the specified list of tags from the specified file's metadata.
 function remove_tag_from_file {
   [ -z "$p_r_path" ] &&  __print_missing_parameter_error "path"
-  [ -z "$p_r_tags" ] &&  __print_missing_parameter_error "tags"
-  tagFilePath="$(dirname $p_r_path)/.$(basename $p_r_path)__tags"
-  IFS=,; read -a tagsArr <<< "$p_r_tags"
+  [ -z "$p_o_tags" ] &&  __print_missing_parameter_error "tags"
+  tagFilePath="$(dirname $p_r_path)/.$(basename $p_r_path)$tagsFilePostfix"
+  IFS=,; read -a tagsArr <<< "$p_o_tags"
   for (( i=0; i<${#tagsArr[@]}; i++ )); do
     sed -i '' "s/$separator${tagsArr[i]}$separator/$separator/g" $tagFilePath # Tags are removed separately as they might not lie consecutively.
   done
@@ -93,12 +100,14 @@ function remove_tag_from_file {
 # Check if input is piped.
 read -t 0.1 inp; # Doesn't read more than a line.
 if [ ! -z "$inp" ]; then
-  p_r_tags="$inp"
+  p_o_tags="$inp"
   while read inp; do
-    p_r_tags+="\n$inp"
+    p_o_tags+="\n$inp"
   done 
 fi
+# Constants
 separator=","
+tagsFilePostfix="__tags"
 # Parse options and parameters.
 while getopts "a:p:t:h" inp; do
   case $inp in
@@ -107,7 +116,7 @@ while getopts "a:p:t:h" inp; do
     ## The path of the file to be tagged or the directory to be searched. 
     p) p_r_path=$OPTARG ;;
     ## A comma-separated list of tags.
-    t) p_r_tags=$OPTARG ;;
+    t) p_o_tags=$OPTARG ;;
     h) __print_help ;;
     *) __print_usage ;;
   esac
